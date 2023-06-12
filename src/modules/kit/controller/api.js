f69e61284bcd74623a72668fcb98ee13215ab68e
Ahmed temp <ahmdzhran50@gmail.com>
2023-06-12 13:15:05 +0300

    code

--- src/modules/kit/controller/api.js
+++ src/modules/kit/controller/api.js
@@ -3,18 +3,18 @@ import { asyncHandler } from "../../../middleware/errorHandling.js";
 import { genToken } from "../../../utils/generateToken.js";
 import { getCompered } from "../../../utils/hashPassword.js";
 
-
-
-// login API 
+// login API
 export const login = asyncHandler(async (req, res, next) => {
-    const { productId, password } = req.body;
-    const product = await productModel.findOne({ productId:productId.toLowerCase() });
-    if (!product) {
-      return next(new Error("in-valid product Id" ,  {cause:404}));
-    }
-    if (!getCompered(password, product.password)) {
-      return next(new Error("in-valid password" ,  {cause:409}));
-    }
-      const data = {...product}
-    return res.json({message:"done" ,data});
+  const { productId, password } = req.body;
+  const product = await productModel.findOne({
+    productId: productId.toLowerCase(),
   });
+  if (!product) {
+    return next(new Error("in-valid product Id", { cause: 404 }));
+  }
+  if (!getCompered(password, product.password)) {
+    return next(new Error("in-valid password", { cause: 409 }));
+  }
+  const data = { ...product._doc };
+  return res.json({ message: "done", data });
+});
