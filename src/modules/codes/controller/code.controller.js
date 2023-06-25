8b54b402d69c7ccbbb19671ce7b6ccc9749cd1c3
Ahmed temp <ahmdzhran50@gmail.com>
2023-06-25 10:21:08 +0300

    code

--- src/modules/codes/controller/code.controller.js
+++ src/modules/codes/controller/code.controller.js
@@ -2,6 +2,7 @@ import { asyncHandler } from "./../../../middleware/errorHandling.js";
 import fs from "fs";
 import { codeModel } from "../../../../DB/models/codes/code.model.js";
 import cloudinary from "./../../../utils/Multer/cloudinary.js";
+import { productModel } from './../../../../DB/models/product/product.model.js';
 
 export const addCode = asyncHandler(async (req, res, next) => {
   const text = fs.readFileSync(req.file.path, "utf8").replace(/\r/g, "");
@@ -13,7 +14,6 @@ export const addCode = asyncHandler(async (req, res, next) => {
     req.file?.path,
     { folder: `files/${req.params?.productId}`, resource_type: "auto" }
   );
-  
   let newCode = await codeModel.create({
     text,
     productId:req.params.productId,
@@ -21,5 +21,6 @@ export const addCode = asyncHandler(async (req, res, next) => {
     description:req.body.description,
     path: { secure_url, public_id },
   });
+  await productModel.updateOne({_id:req.params.productId},{codeId:newCode._id})
   return res.json({ newCode });
 });
