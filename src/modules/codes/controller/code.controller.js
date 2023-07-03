258533710eef873c9dca89fcd7a1757e8cf3d498
Ahmed temp <ahmdzhran50@gmail.com>
2023-07-03 21:33:04 +0300

    cc

--- src/modules/codes/controller/code.controller.js
+++ src/modules/codes/controller/code.controller.js
@@ -5,6 +5,10 @@ import cloudinary from "./../../../utils/Multer/cloudinary.js";
 import { productModel } from "./../../../../DB/models/product/product.model.js";
 
 export const addCode = asyncHandler(async (req, res, next) => {
+  const product =await productModel.findById(req.params.productId);
+  if (!product) {
+    return next(new Error("Invalid product Id "));
+  }
   if (!req.file) {
     return next(new Error("file is required"));
   }
@@ -21,15 +25,18 @@ export const addCode = asyncHandler(async (req, res, next) => {
     version: req.body.version,
     path: { secure_url, public_id },
   });
+  if (!product.codeId) {
+    product.codeId = newCode._id ;
+  }
   return  res.redirect(`/admin/details/${req.params.productId}`);
 });
 
-export const update = asyncHandler(async (req, res, next) => {
-  const { codeId } = req.params;
-  await productModel.updateOne(
-    { _id: req.product._id },
-    { codeId, restartWithCode: true }
-  );
-  req.flash("updating", true);
-  return res.redirect("/profile");
-});
+// export const update = asyncHandler(async (req, res, next) => {
+//   const { codeId } = req.params;
+//   await productModel.updateOne(
+//     { _id: req.product._id },
+//     { codeId, restartWithCode: true }
+//   );
+//   req.flash("updating", true);
+//   return res.redirect("/profile");
+// });
